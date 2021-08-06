d6ee093e95ab361a8f32d600d3955660d68e7730
Pedro Luis Artunduaga Ortiz <61562230+plao2411p@users.noreply.github.com>
2021-08-06 01:15:38 -0500

    create checkparentheses

--- app/app.js
+++ app/app.js
@@ -5,12 +5,12 @@ var string;
 var lengthString;
 /*ending on N means Number*/
 const operationsN = {
-    un : 0,
-    in : 0,
-    di : 0,
-    ds : 0,
-    nTotal : 0,
-    psOfPaN : 0 // Pairs of parentheses N
+    un: 0,
+    in: 0,
+    di: 0,
+    ds: 0,
+    nTotal: 0,
+    psOfPaN: 0 // Pairs of parentheses N
 }
 
 /*Html elements to intetact*/
@@ -33,13 +33,13 @@ button.onclick = () => {
     } else {
         outPut.innerHTML = string;
     }
-    
+
 }
 
 /*String process*/
 
 function stringDepure() {
-    
+
     const noNumber = () => {
         for (let i = 0; i < 10; i++) {
             if (string.includes(i)) {
@@ -85,8 +85,67 @@ function stringDepure() {
         if (lengthString == 1) {
             return true;
         }
+        var leftCount = 0;
+        var rightCount = 0;
+        operationsN.psOfPaN = string.split("(").length + string.split(")").length - 2;
+        if (operationsN.psOfPaN == 0 && operationsN.nTotal == 1) {
+            return true;
+        } else if (operationsN.psOfPaN % 2 == 0) {
+            operationsN.psOfPaN /= 2;
+            if (operationsN.psOfPaN + 1 != operationsN.nTotal) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        if (string[0] == "(") {
+            if (sets.includes(string[1]) || string[1] == "(") {
+                rightCount += 1;
+            } else {
+                return false;
+            }
+        }
+        if (string[1] == "(") {
+            if (string[0] == "(") {
+                rightCount += 1;
+            } else {
+                return false;
+            }
+        }
+        if (string[lengthString - 1] == ")") {
+            if (sets.includes(string[lengthString - 2]) || string[lengthString - 2] == ")") {
+                leftCount += 1;
+            } else {
+                return false;
+            }
+        }
+        if (string[lengthString - 2] == ")") {
+            if (string[lengthString - 1] == ")") {
+                leftCount += 1;
+            }
+        }
+        for (let i = 2; i < lengthString - 2; i++) {
+            if (string[i] == "(") {
+                if (string[i + 1] == "(" || sets.includes(string[i + 1])) {
+                } else { return false; }
+                if (string[i - 1] == "(" || operations.includes(string.slice(i - 2, i))) {
+                } else { return false; }
+                rightCount += 1;
+            } else if (string[i] == ")") {
+                if (string[i - 1] == ")" || sets.includes(string[i - 1])) {
+                } else { return false; }
+                if (string[i + 1] == ")" || operations.includes(string.slice(i + 1, i + 3))) {
+                } else { return false; }
+                leftCount += 1
+            }
+        }
+        console.log("le " + leftCount, "ri " + rightCount)
+        if (leftCount + rightCount == 2 * operationsN.psOfPaN) {
+            return true;
+        } else { return false; }
     }
-    const checkOperations = () =>{
+
+    const checkOperations = () => {
         if (lengthString == 1) {
             return true;
         }
@@ -94,7 +153,12 @@ function stringDepure() {
 
     if (noNumber()) {
         if (withOperations()) {
-            return true
+            if (checkParentheses()) {
+                return true;    
+            } else {
+                string = "Mal ingreso de los parentesis o conjunto";
+                return false;
+            }
         } else {
             string = "No ingresó una operación valida";
             return false
@@ -108,33 +172,3 @@ function stringDepure() {
 function stringOperations() {
     /*Cuerpo de la función*/
 }
-
-
-
-
-
-/**
-button.addEventListener("click", () => {
-    outPut.innerHTML = inPut.value;
-    inPut.onfocus = () => {
-        inPut.value = "";
-    }
-
-});
-const impre = () => {
-    let v = { name: "Pedro", age: 20 };
-    console.log(v);
-}
-impre();
-/* commit Object : propierties and methods
-const person = {
-    name: "",
-    age: "",
-    id: "",
-    metodo: function () {
-        return this.name + "" + this.id;
-    }
-};
-person.name = "Casa";
-console.log(person.name);
-**/
